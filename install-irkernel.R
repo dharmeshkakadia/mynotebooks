fca82100b0f27a971181900ea5b86a8828574666
Dharmesh Kakadia <dharmeshkakadia@users.noreply.github.com>
2016-07-13 15:24:38 -0700

    Create install-irkernel.R

---
+++ install-irkernel.R
@@ -0,0 +1,8 @@
+
+#from https://github.com/ctb/2016-mybinder-irkernel
+
+install.packages(c('rzmq','repr','IRkernel','IRdisplay'),
+                 repos = c('http://irkernel.github.io/',
+                           'http://cran.us.r-project.org'))
+
+IRkernel::installspec()
